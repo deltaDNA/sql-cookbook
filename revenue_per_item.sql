128cb251be991ce38278475706ed640de25ac1d5
StevenStip <steven.vanstiphout@deltadna.com>
2017-03-09 13:27:40 +0000

    revenue per item added

---
+++ revenue_per_item.sql
@@ -0,0 +1,23 @@
+--Real currency spent, grouped by item.
+--combine different child events from transaction event
+WITH items as
+  (SELECT productAmount, ItemName, mainEventId
+   FROM events
+   WHERE eventName = 'transaction'
+     AND productCategory = 'ITEM'),
+     spendings AS
+  (SELECT i.ItemName,
+          i.productAmount,
+          i.mainEventId,
+          e.convertedProductAmount
+   FROM events AS e
+   INNER JOIN items AS i ON e.mainEventId = i.mainEventId
+   WHERE e.productCategory = 'REAL_CURRENCY'
+   ORDER BY itemName)
+SELECT itemName,
+       sum(productAmount) as 'items sold',
+       round(sum(convertedProductAmount)/100,2.0) as revenue,
+       count(*) as sales
+FROM spendings
+GROUP BY itemName
+order by "revenue" desc
