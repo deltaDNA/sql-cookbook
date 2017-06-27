8109c6fd10a4f193fa1a4aac0d33e651ec3e96f0
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2017-06-27 14:15:32 +0100

    smartads and iap revenue

---
+++ KPIs/ARPDAU_and_revenue_with_smartads.sql
@@ -0,0 +1,12 @@
+--arpdau and revenue for IAP and SmartAds revenue
+--This query only works if the default currency of your game is USD
+--Keep in mind that the SmartAds revenue is an estimate based on previous performance.
+select eventdate,
+round(
+    (sum(convertedproductAmount)+sum(adEcpm/1000))/100/count(distinct userid)
+,4)::float as ARPDAU,
+(sum(convertedproductAmount)+sum(adEcpm)/1000)/100::float as 'revenue for both IAP and SmartAds'
+from events
+where eventdate between current_date -30 and current_date
+group by eventdate
+order by eventdate desc
