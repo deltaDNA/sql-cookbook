9d412914773e8e04c64cbc51109599f14672169b
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2017-07-04 10:37:51 +0100

    added ftd query

---
+++ KPIs/First_IAP_by_transactionName.sql
@@ -0,0 +1,16 @@
+--First deposit by transactionName
+with transactions as (
+select 
+	userId, eventTimestamp,transactionName, convertedproductAmount, revenueValidated, 
+	rank() over (partition by userid order by eventTimestamp) as transactionNumber
+from events 
+where convertedProductAmount>0 and revenueValidated in (0,1)
+)
+select
+	transactionName,
+	count(*) as 'IAP count',
+	sum(convertedproductAmount)/100::float as Revenue
+from transactions
+where transactionNumber = 1
+group by transactionName
+order by 2 desc
