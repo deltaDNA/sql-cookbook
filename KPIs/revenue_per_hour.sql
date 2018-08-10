c9c6ec5c8e4ad73b9eb3acdf1ccf122a1432c8a3
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2018-08-10 17:03:24 +0100

    added revenue per hour

---
+++ KPIs/revenue_per_hour.sql
@@ -0,0 +1,12 @@
+/* Get the timestamp and round it down to the hour,
+then get the revenue for this hour, the total number of users as well as the total number of spenders within this window.
+*/
+select date_trunc('hour', eventTimestamp) as time,
+to_char(sum(convertedproductamount)/100, '$999,999,999,999.00') as revenue,
+count(distinct userid) active_users,
+count(distinct case when convertedproductamount>0 then userId end) as spenders
+from events
+where revenuevalidated not in (2,3)
+and eventName in ('gameStarted', 'transaction')
+and eventTimestamp between now() - interval '100 hours' and now()
+group by 1 order by 1 desc
