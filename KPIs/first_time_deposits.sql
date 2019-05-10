755d5cce86913c3e51c41db95f76541630302cdc
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2019-05-10 15:26:59 +0100

    fixed query

--- KPIs/first_time_deposits.sql
+++ KPIs/first_time_deposits.sql
@@ -2,5 +2,6 @@
 select  firstPaymentTimestamp::date as spend_date, 
         count(*) as spenders
 from user_metrics
+where firstPaymentTimestamp is not null
 group by 1 
-order by 1
+order by 1 desc
