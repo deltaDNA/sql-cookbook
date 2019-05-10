b3d199dee431e47e04ea8093e7c2a6185bcd9056
Steven van Stiphout <steven.vanstiphout@deltadna.com>
2019-05-10 15:21:03 +0100

    spender percentage and ftd query

---
+++ KPIs/first_time_deposits.sql
@@ -0,0 +1,6 @@
+/* get the date of the first payment and count the number of users */
+select  firstPaymentTimestamp::date as spend_date, 
+        count(*) as spenders
+from user_metrics
+group by 1 
+order by 1
---
+++ KPIs/spender_percentage.sql
@@ -0,0 +1,18 @@
+/*Gets the number of players who have ever spent and the number of players active.
+*/
+with dailyAggregates as (
+select  event_date, 
+        user_id, 
+        sum(revenue) as revenue,
+        sum(sum(revenue)) over (partition by user_id order by event_date) as cumulative_revenue
+from fact_user_sesssions_day
+group by event_date, user_id
+)
+select  event_date,
+        count(distinct user_id) as DAU,
+        count(distinct case when revenue>0 then user_id end) as spenders_today,
+        count(distinct case when revenue>0 then user_id end)/count(distinct user_id) as percentage_spending_today,
+        count(distinct case when cumulative_revenue>0 then user_id end) as active_spenders,
+        count(distinct case when cumulative_revenue>0 then user_id end)/count(distinct user_id) as percentage_spenders_active
+from  dailyAggregates   
+group by 1
